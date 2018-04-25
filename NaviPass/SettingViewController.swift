3c8950242deaa6ca75494e0f700d747dd30b0365
ちょこばた <support@c5bt.net>
2018-04-25 22:58:57 +0900

    [add] strings file and view controller code

--- NaviPass/SettingViewController.swift
+++ NaviPass/SettingViewController.swift
@@ -8,28 +8,46 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var SettingTitle = [ [NSLocalizedString("settingColor", comment: ""), NSLocalizedString("settingColor1", comment: ""), NSLocalizedString("settingColor2", comment: ""), NSLocalizedString("settingColor3", comment: "")],
+                       [NSLocalizedString("settingRemoveAd", comment: ""), NSLocalizedString("settingPurchase", comment: "")],
+                       [NSLocalizedString("settingSupport", comment: ""), "Twitter", NSLocalizedString("settingSupport2", comment: "")]
+    ]
+    
+    var SettingSubtitle = [ ["", NSLocalizedString("settingColor1E", comment: ""), NSLocalizedString("settingColor2E", comment: ""), NSLocalizedString("settingColor3E", comment: "")],
+                          ["", NSLocalizedString("settingPurchaseE", comment: "")],
+                          ["", NSLocalizedString("settingSupport1E", comment: ""), NSLocalizedString("settingSupport2E", comment: "")]
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - テーブル設定
+    //参照：https://qiita.com/Ryosuke-Hujisawa/items/d2dcc260087230345acb
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return SettingTitle.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return SettingTitle[section].count - 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.value1, reuseIdentifier: "cell")
+        cell.textLabel?.text = SettingTitle[indexPath.section][indexPath.row + 1]
+        cell.detailTextLabel?.text = SettingSubtitle[indexPath.section][indexPath.row + 1]
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return SettingTitle[section][0]
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
 
 }
